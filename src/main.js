d3a55daf0956684099030cce86632963c5fa201b
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-08-04 16:58:38 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,38 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Vue.config.productionTip = false;
 
 store.dispatch('autoLogin').then(() => {
   new Vue({

9c3d7070113b44c9b8c9be543be10e6a7bbd6e51
kohei <kohei.omachi@gmail.com>
2020-08-09 14:20:57 +0900

    revert

--- src/components/pages/MyPage.vue
+++ src/components/pages/MyPage.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <h2>マイページ</h2>
-
     <p> {{userName}}さんの新着投稿</p>
-
-    <Posts v-bind:posts='individualNewPosts'></Posts>
+    <h3>投稿一覧</h3>
+    <hr>
+    <div v-for="post in individualNewPosts" :key="post.name" class="content-box">
+      <div class="title">{{post.document.fields.title.stringValue}}</div>
+      <div>
+        <router-link to="/my-page">
+          {{post.document.fields.userName.stringValue}}
+        </router-link>
+      </div>
+      <div>{{post.document.fields.major.stringValue}} {{post.document.fields.grade.stringValue}}</div>
+      <div class="content box">{{post.document.fields.content.stringValue}}</div>
+      <div>カテゴリ：{{post.document.fields.category.stringValue}}</div>
+      <div>投稿時間：{{post.document.fields.created_at.timestampValue | dateFormat}}</div>
+      <!-- <div>投稿者：{{post.document.fields.userName.timestampValue}}</div> -->
+      <!-- <div>編集時間：{{post.document.fields.updated_at.timestampValue}}</div> -->
+      <!-- <div>回答有無：{{post.document.fields.isAnswered.booleanValue}}</div> -->
+      <br>
+    </div>
   </div>
 </template>
 
 
 <script>
-import Posts from './Posts';
 export default {
   computed: {
     userName() {
@@ -23,8 +37,28 @@ export default {
   created() {
     this.$store.dispatch('contents/getIndividualPosts');
   },
-  components: {
-    Posts
-  }
 }
 </script>
+<style scoped>
+div .title {
+  font-size: larger;
+  font-weight: bolder;
+  padding-top: 15px;
+}
+
+.box{
+  width: 50%;
+  padding: 0.5em 1em;
+  margin: auto;
+  background: #edeef0;
+  color: rgb(231, 9, 9);
+}
+
+.content-box {
+  background-color: rgb(251, 242, 209);
+  border-radius: 9px;
+  width: 70%;
+  margin: 20px auto;
+}
+
+</style>

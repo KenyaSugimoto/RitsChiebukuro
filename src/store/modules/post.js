54a79268d5db588e834ee956803bbdf0c2a63268
kohei <kohei.omachi@gmail.com>
2020-08-09 23:15:31 +0900

    clean
    インデント修正

--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -30,13 +30,140 @@ const createdAtDesc = [
 
 const actions = {
   getPosts({ rootGetters, commit }) {
-    axiosQuery
-      .post(
-        "/documents:runQuery",
+    axiosQuery.post("/documents:runQuery",
+      {
+        structuredQuery: {
+          select: {
+            fields
+          },
+          from,
+          orderBy: createdAtDesc,
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${rootGetters.idToken}`,
+        },
+      }
+    ).then((response) => {
+      commit("updateNewPosts", response.data, {root: true});
+    }).catch((error) => {
+      console.log(error);
+    });
+  },
+  createPost({ rootGetters }, postData) {
+    axiosDb.post("/posts/",
+      {
+        fields: {
+          postId: {
+            stringValue: ''
+          },
+          title: {
+            stringValue: postData.title
+          },
+          content: {
+            stringValue: postData.content
+          },
+          category: {
+            stringValue: postData.category
+          },
+          isAnswered: {
+            booleanValue: false
+          },
+          uid: {
+            stringValue: rootGetters.uid
+          },
+          userName: {
+            stringValue: rootGetters.userName
+          },
+          major: {
+            stringValue: rootGetters.major
+          },
+          grade: {
+            stringValue: rootGetters.grade
+          },
+          created_at: {
+            timestampValue: new Date().toISOString()
+          },
+          updated_at: {
+            timestampValue: new Date().toISOString()
+          },
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${postData.idToken}`,
+        },
+      }
+    ).then((response) => {
+      const documentId = response.data.name.split('/')[6];
+      axiosDb.patch(`posts/${documentId}?updateMask.fieldPaths=postId`,
+        {
+          fields: {
+            postId: {stringValue: documentId}
+          }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${postData.idToken}`,
+          },
+        }
+      );
+    }).catch((error) => {
+      console.log(error);
+    });
+  },
+  getIndividualPosts({rootGetters, commit}) {
+    axiosQuery.post("/documents:runQuery",
+      {
+        structuredQuery: {
+          select: {
+            fields,
+          },
+          from,
+          orderBy: createdAtDesc,
+          where: {
+            compositeFilter: {
+              op: "AND",
+              filters: [
+                {
+                  fieldFilter: {
+                    field: {
+                      fieldPath: "uid",
+                    },
+                    op: "EQUAL",
+                    value: {
+                      stringValue: rootGetters.uid,
+                    }
+                  },
+                }
+              ],
+            }
+          },
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${rootGetters.idToken}`,
+        },
+      }
+    ).then((response) => {
+      if ("document" in response.data[0]) {
+        commit("updateIndividualNewPosts", response.data, {root: true});
+      } else {
+        commit("updateIndividualNewPosts", null, {root: true});
+      }
+    }).catch((error) => {
+      console.log(error.response);
+    });
+  },
+  getSelectedCategoryNewPosts({rootGetters, commit}, category) {
+    if (!category) {
+      axiosQuery.post("/documents:runQuery",
         {
           structuredQuery: {
             select: {
-              fields
+              fields,
             },
             from,
             orderBy: createdAtDesc,
@@ -47,77 +174,17 @@ const actions = {
             Authorization: `Bearer ${rootGetters.idToken}`,
           },
         }
-      )
-      .then((response) => {
-        commit("updateNewPosts", response.data, {root: true});
-      });
-  },
-  createPost({ rootGetters }, postData) {
-    axiosDb
-      .post(
-        "/posts/",
-        {
-          fields: {
-            postId: {
-              stringValue: ''
-            },
-            title: {
-              stringValue: postData.title
-            },
-            content: {
-              stringValue: postData.content
-            },
-            category: {
-              stringValue: postData.category
-            },
-            isAnswered: {
-              booleanValue: false
-            },
-            uid: {
-              stringValue: rootGetters.uid
-            },
-            userName: {
-              stringValue: rootGetters.userName
-            },
-            major: {
-              stringValue: rootGetters.major
-            },
-            grade: {
-              stringValue: rootGetters.grade
-            },
-            created_at: {
-              timestampValue: new Date().toISOString()
-            },
-            updated_at: {
-              timestampValue: new Date().toISOString()
-            },
-          },
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${postData.idToken}`,
-          },
-        }
       ).then((response) => {
-        const documentId = response.data.name.split('/')[6];
-        axiosDb.patch(`posts/${documentId}?updateMask.fieldPaths=postId`,
-          {
-            fields: {
-              postId: {stringValue: documentId}
-            }
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${postData.idToken}`,
-            },
-          }
-        );
+        if ("document" in response.data[0]) {
+          commit("updateSelectedCategoryNewPosts", response.data, {root: true});
+        } else {
+          commit("updateSelectedCategoryNewPosts", null, {root: true});
+        }
+      }).catch((error) => {
+        console.log(error.response);
       });
-  },
-  getIndividualPosts({rootGetters, commit}) {
-    axiosQuery
-      .post(
-        "/documents:runQuery",
+    } else {
+      axiosQuery.post("/documents:runQuery",
         {
           structuredQuery: {
             select: {
@@ -132,11 +199,11 @@ const actions = {
                   {
                     fieldFilter: {
                       field: {
-                        fieldPath: "uid",
+                        fieldPath: "category",
                       },
                       op: "EQUAL",
                       value: {
-                        stringValue: rootGetters.uid,
+                        stringValue: category,
                       }
                     },
                   }
@@ -150,91 +217,15 @@ const actions = {
             Authorization: `Bearer ${rootGetters.idToken}`,
           },
         }
-      )
-      .then((response) => {
+      ).then((response) => {
         if ("document" in response.data[0]) {
-          commit("updateIndividualNewPosts", response.data, {root: true});
+          commit("updateSelectedCategoryNewPosts", response.data, {root: true});
         } else {
-          commit("updateIndividualNewPosts", null, {root: true});
+          commit("updateSelectedCategoryNewPosts", null, {root: true});
         }
       }).catch((error) => {
         console.log(error.response);
       });
-  },
-  getSelectedCategoryNewPosts({rootGetters, commit}, category) {
-    if (!category) {
-      axiosQuery
-        .post(
-          "/documents:runQuery",
-          {
-            structuredQuery: {
-              select: {
-                fields,
-              },
-              from,
-              orderBy: createdAtDesc,
-            },
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${rootGetters.idToken}`,
-            },
-          }
-        )
-        .then((response) => {
-          if ("document" in response.data[0]) {
-            commit("updateSelectedCategoryNewPosts", response.data, {root: true});
-          } else {
-            commit("updateSelectedCategoryNewPosts", null, {root: true});
-          }
-        }).catch((error) => {
-          console.log(error.response);
-        });
-    } else {
-      axiosQuery
-        .post("/documents:runQuery",
-          {
-            structuredQuery: {
-              select: {
-                fields,
-              },
-              from,
-              orderBy: createdAtDesc,
-              where: {
-                compositeFilter: {
-                  op: "AND",
-                  filters: [
-                    {
-                      fieldFilter: {
-                        field: {
-                          fieldPath: "category",
-                        },
-                        op: "EQUAL",
-                        value: {
-                          stringValue: category,
-                        }
-                      },
-                    }
-                  ],
-                }
-              },
-            },
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${rootGetters.idToken}`,
-            },
-          }
-        )
-        .then((response) => {
-          if ("document" in response.data[0]) {
-            commit("updateSelectedCategoryNewPosts", response.data, {root: true});
-          } else {
-            commit("updateSelectedCategoryNewPosts", null, {root: true});
-          }
-        }).catch((error) => {
-          console.log(error.response);
-        });
     }
   },
   async deletePost({rootGetters}, post) {

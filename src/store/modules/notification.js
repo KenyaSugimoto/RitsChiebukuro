afad3834f407430494a49ddfc60ee3fb0ae4017a
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-08-12 12:26:40 +0900

    add

--- src/store/modules/notification.js
+++ src/store/modules/notification.js
@@ -1,5 +1,4 @@
 import axiosDb from '../../axios/axios-db';
-// import axiosQuery from '../../axios/axios-query';
 
 
 const actions = {
@@ -13,8 +12,8 @@ const actions = {
       commit("updateNotifications", response.data.fields, {root: true});
     });
   },
-  getQuestionerNotifications({rootGetters, commit}) {
-    axiosDb.get(
+  async updateQuestionerNotifications({rootGetters, commit}) {
+    await axiosDb.get(
       `/notifications/${rootGetters.watchingPost.document.fields.uid.stringValue}`,
       {
         headers: {Authorization: `Bearer ${rootGetters.idToken}`,},
@@ -23,24 +22,19 @@ const actions = {
       commit("updateQuestionerNotifications", response.data.fields, {root: true});
     });
   },
-  addNotification({rootGetters, dispatch}, notificationData) {
-    const key = notificationData.notificationId.stringValue;
-    const key2 = new Date().getTime().toString(16) + Math.floor(1000*Math.random()).toString(16);
+  addNotification({rootGetters, commit, dispatch}, notificationData) {
+    const objectKey = notificationData.notificationId.stringValue;
+    const newNotificationData = {
+      [objectKey]: {
+        mapValue: {
+          fields: notificationData
+        }
+      }
+    };
     axiosDb.post(
       `/notifications?documentId=${notificationData.questionerUid.stringValue}`,
       {
-        fields: {
-          [key]: {
-            mapValue: {
-              fields: notificationData
-            }
-          },
-          [key2]: {
-            mapValue: {
-              fields: notificationData
-            }
-          },
-        }
+        fields: newNotificationData
       },
       {
         headers: {
@@ -52,14 +46,64 @@ const actions = {
     }).catch(error => {
       const errorStatus = error.response.data.error.status;
       if (errorStatus == "ALREADY_EXISTS") {
-        console.log("キャッチ成功");
-        // vuexに質問者の通知情報を格納成功
-        dispatch("notification/getQuestionerNotifications", {}, {root: true});
-        // 今回の通知情報を追加
-
-        // 追加後再送
-      }else {
-        console.log("キャッチ失敗");
+        dispatch("notification/updateQuestionerNotifications", {}, {root: true})
+        .then(() => {
+          const nowQuestionerNotifications = rootGetters.questionerNotifications;
+          nowQuestionerNotifications[objectKey] = newNotificationData[objectKey];
+          commit("updateQuestionerNotifications", null, {root: true});
+          commit("updateQuestionerNotifications", nowQuestionerNotifications, {root: true});
+          const updatedQuestionerNotifications = rootGetters.questionerNotifications;
+          var sendObject = {};
+          Object.keys(updatedQuestionerNotifications).forEach(function(keyData) {
+            const item = {
+              notificationId: {
+                stringValue: updatedQuestionerNotifications[keyData].mapValue.fields.notificationId.stringValue
+              },
+              created_at: {
+                timestampValue: updatedQuestionerNotifications[keyData].mapValue.fields.created_at.timestampValue
+              },
+              respondentName: {
+                stringValue: updatedQuestionerNotifications[keyData].mapValue.fields.respondentName.stringValue
+              },
+              threadId: {
+                stringValue: updatedQuestionerNotifications[keyData].mapValue.fields.threadId.stringValue
+              },
+              type: {
+                stringValue: updatedQuestionerNotifications[keyData].mapValue.fields.type.stringValue
+              },
+              questionerUid: {
+                stringValue: updatedQuestionerNotifications[keyData].mapValue.fields.questionerUid.stringValue
+              },
+            };
+            sendObject[item.notificationId.stringValue] = {
+              mapValue: {
+                fields: item
+              }
+            };
+          });
+          axiosDb.delete(
+            `/notifications/${notificationData.questionerUid.stringValue}`,
+            {
+              headers: {
+                Authorization: `Bearer ${rootGetters.idToken}`,
+              },
+            }
+          ).then(doc => {
+            console.log("Document successfully deleted!",doc);
+            axiosDb.post(
+              `/notifications?documentId=${notificationData.questionerUid.stringValue}`,
+              {
+                fields: sendObject
+              },
+              {
+                headers: {
+                  Authorization: `Bearer ${rootGetters.idToken}`,
+                },
+              }).then(response => {
+                console.log(response);
+              });
+          });
+        });
       }
     });
   },

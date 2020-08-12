5fb79592bb9dab012cb734d824c6e054142ad75d
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-08-13 07:30:35 +0900

    fix

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -37,7 +37,7 @@ export default {
     },
     getNotification() {
       this.$store.dispatch("notification/getNotifications");
-      this.$router.push('/notification');
+      this.$router.push('/notification').catch(() => {});
 
     },
   },

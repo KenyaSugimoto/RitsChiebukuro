74cf2149574e4d48701c565afe80aa3b7dcece93
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2020-08-12 10:36:50 +0900

    クリック時のページ遷移実装

--- src/components/parts/Notification.vue
+++ src/components/parts/Notification.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
     <h2>通知</h2>
-    <div v-for='item in notifications' :key="item.mapValue.fields.notificationId.stringValue" class='content-box'>
-      <div>
-        <p>{{item.mapValue.fields.notificationId.stringValue}}</p>
-        あなたの投稿に対して{{item.mapValue.fields.respondentName.stringValue}}さんが
-        <div v-if='item.mapValue.fields.type.stringValue == "answer"'>回答しました</div>
+    <div
+      v-for="item in notifications"
+      :key="item.mapValue.fields.notificationId.stringValue"
+      class="content-box"
+    >
+      <div @click="toPost(item)" class="notification-link">
+        <p>{{ item.mapValue.fields.notificationId.stringValue }}</p>
+        あなたの投稿に対して{{
+          item.mapValue.fields.respondentName.stringValue
+        }}さんが
+        <div v-if="item.mapValue.fields.type.stringValue == 'answer'">
+          回答しました
+        </div>
         <div v-else>コメントしました</div>
         <!-- <p>経過時刻: {{elapsedTime(item)}} </p> -->
-        <p>投稿時刻: {{item.mapValue.fields.created_at.timestampValue | dateFormat}} </p>
+        <p>
+          投稿時刻:
+          {{ item.mapValue.fields.created_at.timestampValue | dateFormat }}
+        </p>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -29,13 +39,33 @@ export default {
     //   // return now.getTime() - Date.parse(notification.mapValue.fields.created_at.timestampValue);
     //   // return
     // },
-  }
-
-}
+  },
+  methods: {
+    toPost(notification) {
+      this.$store.dispatch(
+        "post/getPostByPostId",
+        notification.mapValue.fields.threadId.stringValue
+      );
+      this.$router.push({
+        name: "post",
+        params: { postId: notification.mapValue.fields.threadId.stringValue },
+      });
+      //通知の削除
+    },
+  },
+};
 </script>
 
 <style scoped>
 .content-box {
   background-color: rgb(206, 220, 221);
+}
+.notification-link {
+  padding: 10px;
+  cursor: pointer;
+}
+.notification-link:hover {
+  background-color: #b1abab;
+  color: red;
 }
 </style>

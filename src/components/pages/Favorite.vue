2be77d626707f0646df40288600c2a899bffbfdc
kohei <kohei.omachi@gmail.com>
2020-08-15 16:45:13 +0900

    add

---
+++ src/components/pages/Favorite.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{userName}}さんの気になる質問</h2>
+
+    <Posts v-bind:posts='newPosts | favorite(favoritePostIds) | acceptingAnswer' name='回答受付中の質問'></Posts>
+    <Posts v-bind:posts='newPosts | favorite(favoritePostIds) | manyViews' name='よく見られている質問'></Posts>
+    <Posts v-bind:posts='newPosts | favorite(favoritePostIds) | resolved' name='解決済みの質問'></Posts>
+  </div>
+</template>
+
+<script>
+import Posts from './Posts';
+export default {
+  computed: {
+    userName() {
+      return this.$store.getters.userName;
+    },
+    newPosts() {
+      return this.$store.getters.newPosts;
+    },
+    favoritePostIds() {
+      return this.$store.getters.favoritePostIds;
+    },
+  },
+  components: {
+    Posts
+  }
+}
+</script>
---
+++ src/components/pages/MyPosts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{userName}}さんの質問</h2>
+
+    <Posts v-bind:posts='newPosts | user(uid) | acceptingAnswer' name='回答受付中の質問'></Posts>
+    <Posts v-bind:posts='newPosts | user(uid) | manyViews'  name='よく見られている質問'></Posts>
+    <Posts v-bind:posts='newPosts | user(uid) | resolved' name='解決済みの質問'></Posts>
+  </div>
+</template>
+
+<script>
+import Posts from './Posts';
+export default {
+  computed: {
+    userName() {
+      return this.$store.getters.userName;
+    },
+    newPosts() {
+      return this.$store.getters.newPosts;
+    },
+    uid() {
+      return this.$store.getters.uid;
+    },
+  },
+  components: {
+    Posts
+  }
+}
+</script>

8f5147780b3bda785cc824c5e6186f1a3357d7ec
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-08-16 19:59:57 +0900

    途中

--- src/components/pages/NotificationConfig.vue
+++ src/components/pages/NotificationConfig.vue
@@ -4,24 +4,46 @@
     <p>内容を編集して「保存」ボタンを押してください。</p>
     <div class="select-config">
       <br>
-
       <div>
-        <input type="checkbox" name="config" value="1" id="config1">
-        <label for="config1">あなたの質問に対する回答やコメントについて通知</label>
+        <div>
+          <input type="checkbox" value="forQuestioner" id="forQuestioner" v-model="notificationConfigValues">
+          <label for="forQuestioner">あなたの質問に対する回答やコメントについて通知</label>
+        </div>
+        <br>
+        <div>
+          <input type="checkbox" value="forRespondent" id="forRespondent" v-model="notificationConfigValues">
+          <label for="forRespondent">あなたが回答した質問のコメントについて通知</label>
+        </div>
+        <br>
       </div>
-      <br>
-      <div>
-        <input type="checkbox" name="config" value="2" id="config2">
-        <label for="config2">あなたが回答した質問のコメントについて通知</label>
-      </div>
-      <br>
     </div>
-    <br>
 
-    <button>保存</button>
+    <br>
+    <button @click="setConfig">保存</button>
+
+      <p> {{notificationConfigValues}} </p>
+
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      notificationConfigValues: this.nowNotificationConfigValues,
+    }
+  },
+
+  methods: {
+    setConfig() {
+      const nowConfig = this.notificationConfigValues;
+      this.$store.commit("updateNotificationConfigValues", nowConfig);
+      console.log("nowConfig",nowConfig);
+    },
+  }
+}
+</script>
 
 <style scoped>
 button {

4b9bf8afeba9bf462b03f62597672b11b4365f1f
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-09-02 17:16:30 +0900

    fix

--- src/components/pages/SearchResult.vue
+++ src/components/pages/SearchResult.vue
@@ -2,18 +2,27 @@
   <div>
     <br>
     <template v-if='searchResultPosts != null'>
-      <h2>「{{searchKeyWords}}」 の検索結果</h2>
+      <h2>「{{searchKeyWords}}」 の検索結果 <v-icon large>mdi-magnify</v-icon> </h2>
 
       <v-container>
-        <v-row>
-          <v-col>
-            <Posts v-bind:posts='searchResultPosts | acceptingAnswer' name='回答受付中の質問'></Posts>
+        <v-row justify="center">
+          <v-col cols="11">
+            <v-tabs
+              background-color="#9a0002"
+              dark
+              v-model="tab"
+              grow
+              show-arrows
+            >
+              <v-tab>回答受付中の質問<v-icon x-large>mdi-comment-question</v-icon></v-tab>
+              <v-tab>よく見られている質問<v-icon x-large>mdi-account-group</v-icon></v-tab>
+              <v-tab>解決済みの質問<v-icon x-large>mdi-frequently-asked-questions</v-icon></v-tab>
+            </v-tabs>
           </v-col>
-          <v-col>
-            <Posts v-bind:posts='searchResultPosts | manyViews' name='よく見られている質問'></Posts>
-          </v-col>
-          <v-col>
-            <Posts v-bind:posts='searchResultPosts | resolved' name='解決済みの質問'></Posts>
+          <v-col cols="12" sm="12" md="9" lg="9" >
+            <div v-if="tab===0"><Posts v-bind:posts='searchResultPosts | acceptingAnswer' name='回答受付中の質問' iconName="mdi-comment-question"></Posts></div>
+            <div v-if="tab===1"><Posts v-bind:posts='searchResultPosts | manyViews' name='よく見られている質問' iconName="mdi-account-group"></Posts></div>
+            <div v-if="tab===2"><Posts v-bind:posts='searchResultPosts | resolved' name='解決済みの質問' iconName="mdi-frequently-asked-questions"></Posts></div>
           </v-col>
         </v-row>
       </v-container>
@@ -29,6 +38,11 @@
 <script>
 import Posts from './Posts';
 export default {
+  data() {
+    return {
+      tab: null,
+    }
+  },
   computed: {
     searchResultPosts() {
       return this.$store.getters.searchResultPosts;

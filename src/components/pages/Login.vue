218946a54986dccfce0b19b6e56627d53fe5fb38
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-09-06 12:48:06 +0900

    Update Login.vue

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,36 +1,60 @@
 <template>
   <div id="app">
-    <v-text-field class='text-field' label='Email' v-model='email' suffix='@ed.ritsumei.ac.jp' :rules="[rules.email]"></v-text-field>
+    <v-container>
 
-    <br><br>
+      <v-row><v-col><h2>ログイン</h2></v-col></v-row>
 
-    <v-text-field
-        class='text-field'
-        v-model="password"
-        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-        :rules="[rules.min]"
-        :type="show1 ? 'text' : 'password'"
-        label="パスワード"
-        counter
-        @click:append="show1 = !show1"
-      ></v-text-field>
+      <v-row justify="center">
+        <v-col cols="11" lg="6" md="8" sm="9">
+          <v-text-field class='text-field' label='Email' v-model='email' suffix='@ed.ritsumei.ac.jp' :rules="[rules.email]" />
+        </v-col>
+      </v-row>
 
-    <br><br>
-    <br><br>
+      <v-row justify="center">
+        <v-col cols="11" lg="6" md="8" sm="9">
+          <v-text-field
+            class='text-field'
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            label="パスワード"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-btn width="280" height="40" color="#B3424A" @click="login"><font color='white'><b>ログイン</b></font></v-btn>
+      <v-row justify="center">
+        <v-col cols="11" lg="6" md="8" sm="9">
+          <v-btn width="280" height="40" color="#B3424A" @click="login">
+            <font color='white'><b>ログイン</b></font>
+          </v-btn>
+        </v-col>
+      </v-row>
 
-    <br><br>
-    <hr class="line">
+      <br>
+      <v-row justify="center"><v-col cols="11" lg="6" md="8" sm="9"><hr></v-col></v-row>
+      <br><br>
 
-    <p>または</p>
+      <v-row justify="center">
+        <v-col cols="11" lg="6" md="8" sm="9">
+          <p>または</p>
+          <v-btn outlined width="160" height="30" color="#B3424A" @click="signUp">
+            <font color='black'><b>ユーザ登録</b></font>
+          </v-btn>
+        </v-col>
+      </v-row>
 
-    <v-btn outlined width="160" height="30" color="#B3424A" @click="signUp"><font color='black'><b>ユーザ登録</b></font></v-btn>
+      <br><br>
 
-    <br><br>
-    <br><br>
+      <v-row justify="center">
+        <v-col cols="11" lg="6" md="8" sm="9">
+          <router-link to="/resetPassword" class="reset-password">パスワードを忘れた方はこちら</router-link>
+        </v-col>
+      </v-row>
 
-    <router-link to="/resetPassword" class="reset-password">パスワードを忘れた方はこちら</router-link>
+    </v-container>
 
   </div>
 </template>
@@ -90,13 +114,5 @@ export default {
 .reset-password:hover {
   background-color: #b1abab;
   color: red;
-}
-.line {
-  width: 280px;
-  margin: 0 auto;
-}
-.text-field {
-  width: 280px;
-  margin: 0 auto;
 }
 </style>

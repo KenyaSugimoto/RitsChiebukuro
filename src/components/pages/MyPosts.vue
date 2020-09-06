665e417e42d8521d40e7f83008b3c8d10afdac05
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-09-06 14:29:14 +0900

    fix

--- src/components/pages/MyPosts.vue
+++ src/components/pages/MyPosts.vue
@@ -47,29 +47,6 @@
           </v-col>
         </v-row>
       </template>
-
-
-<!--
-      <v-row justify="center">
-        <v-col cols="11">
-          <v-tabs
-            background-color="#9a0002"
-            dark
-            v-model="tab"
-            grow
-            show-arrows
-          >
-            <v-tab>回答受付中の質問<v-icon x-large>mdi-comment-question</v-icon></v-tab>
-            <v-tab>よく見られている質問<v-icon x-large>mdi-account-group</v-icon></v-tab>
-            <v-tab>解決済みの質問<v-icon x-large>mdi-frequently-asked-questions</v-icon></v-tab>
-          </v-tabs>
-        </v-col>
-        <v-col cols="12" sm="12" md="9" lg="9" >
-          <div v-if="tab===0"><Posts v-bind:posts='individualNewPosts | acceptingAnswer' name='回答受付中の質問' iconName="mdi-comment-question"></Posts></div>
-          <div v-if="tab===1"><Posts v-bind:posts='individualNewPosts | manyViews' name='よく見られている質問' iconName="mdi-account-group"></Posts></div>
-          <div v-if="tab===2"><Posts v-bind:posts='individualNewPosts | resolved' name='解決済みの質問' iconName="mdi-frequently-asked-questions"></Posts></div>
-        </v-col>
-      </v-row> -->
     </v-container>
 
   </div>

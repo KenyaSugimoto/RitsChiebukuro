a26c084c4bf248e6a1e10718f67c0e6f5c19fcda
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-09-12 18:03:46 +0900

    add

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -1,50 +1,55 @@
 <template>
+  <!-- スマホ画面用レイアウト -->
   <v-container v-if="xs">
+    <!-- ヘッダー画像 -->
     <img src="../../assets/header.jpg" class="img-xs" @click="toHome">
-      <v-row justify="start">
-        <v-col cols="1">
-          <v-menu v-model="menu" offset-x value="false">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn v-bind="attrs" v-on="on" icon><v-icon>mdi-menu</v-icon></v-btn>
-            </template>
-            <v-card width="400px">
 
-              <!-- マイページボタン -->
-              <v-row justify="center">
-                <v-col cols="12">
-                  <v-btn text @click="toMypage" color="black"><v-icon>mdi-account</v-icon>マイページ</v-btn>
-                  <br><hr>
-                </v-col>
-              </v-row>
+    <!-- ハンバーガーメニュー部分 -->
+    <v-row justify="start">
+      <v-col cols="1">
+        <v-menu v-model="menu" offset-x value="false">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn v-bind="attrs" v-on="on" icon><v-icon>mdi-menu</v-icon></v-btn>
+          </template>
+          <v-card width="400px" color="#fff5f0e5">
 
-              <!-- 通知ボタン -->
-              <v-row>
-                <v-col cols="12">
-                  <v-badge
-                    :content="notifications"
-                    :value="notifications"
-                    color="red"
-                    overlap
-                  >
-                    <v-btn text @click="getNotification" color="black"><v-icon>mdi-bell-ring</v-icon> 通知 </v-btn>
-                  </v-badge>
-                  <hr>
-                </v-col>
-              </v-row>
+            <!-- マイページボタン -->
+            <v-row justify="center">
+              <v-col cols="11">
+                <v-btn text @click="toMypage" color="black"><v-icon>mdi-account</v-icon>マイページ</v-btn>
+                <br><hr>
+              </v-col>
+            </v-row>
 
-              <!-- 質問投稿ボタン -->
-              <v-row>
-                <v-col cols="12">
-                  <v-btn text @click="toPostQuestion" color="black"><v-icon>mdi-help</v-icon>質問する</v-btn>
-                </v-col>
-              </v-row>
-            </v-card>
-          </v-menu>
-        </v-col>
-      </v-row>
+            <!-- 通知ボタン -->
+            <v-row justify="center">
+              <v-col cols="11">
+                <v-badge
+                  :content="notifications"
+                  :value="notifications"
+                  color="red"
+                  overlap
+                >
+                  <v-btn text @click="getNotification" color="black"><v-icon>mdi-bell-ring</v-icon> 通知 </v-btn>
+                </v-badge>
+                <hr>
+              </v-col>
+            </v-row>
+
+            <!-- 質問投稿ボタン -->
+            <v-row>
+              <v-col cols="12">
+                <v-btn text @click="toPostQuestion" color="black"><v-icon>mdi-help</v-icon>質問する</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-menu>
+      </v-col>
+    </v-row>
     <hr>
   </v-container>
 
+  <!-- スマホ以外のレイアウト -->
   <v-container v-else>
     <v-navigation-drawer app v-model="drawer" temporary right width=600px color="#e2e6e9">
       <br><br><br><br><br>
@@ -145,5 +150,6 @@ export default {
   width: 70%;
   height: 70%;
   cursor: pointer;
+  color: #fff5f0e5;
 }
 </style>
